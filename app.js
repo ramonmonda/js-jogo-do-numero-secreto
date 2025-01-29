482c53ad5c15c23d8c9f489679abe9574b8b291f
Ramon Monda <60966322+ramonmonda@users.noreply.github.com>
2025-01-29 10:39:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,10 @@ function exibirMensagemInicial() {
     const mensagemBoasVindas = "Seja bem vindo, ao jogo do número secreto!";
     const escolhaFrase = `Escolha um número entre 1 e ${numeroLimite}`;
 
-    // Verifica se o áudio está autorizado
-    const audioPermission = document.getElementById('audioPermission').checked;
-
     // Converte texto em áudio
-    if (audioPermission) {
-        setTimeout(function() {
-            responsiveVoice.speak(mensagemBoasVindas, "Brazilian Portuguese Female", {rate: 1.2});
-            setTimeout(function() {
-                responsiveVoice.speak(escolhaFrase, "Brazilian Portuguese Female", {rate: 1.2});
-            }, 2000);
-        }, 2000);
-    }
-    
+    responsiveVoice.speak(mensagemBoasVindas, "Brazilian Portuguese Female", {rate:1.2});
+    responsiveVoice.speak(escolhaFrase, "Brazilian Portuguese Female", {rate:1.2});
+
     // Exibe mensagens na tela
     exibeTextoNaTela("h1", "Jogo do Número Secreto");
     exibeTextoNaTela("p", escolhaFrase);
